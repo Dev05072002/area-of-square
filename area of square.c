3b53add9a962e6b41176563185e3df8b8054d90f
Dev05072002 <152999395+Dev05072002@users.noreply.github.com>
2023-12-09 11:58:11 +0530

    Add files via upload

---
+++ area of square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+    int area,s;
+    printf("enter the value of one side");
+    scanf("\n%d",&s);
+    area=s*s;
+    printf("\n the area of the square is%d",area);
+    getch();
+    
+}
